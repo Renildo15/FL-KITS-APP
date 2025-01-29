3001272553fef3fba96c2a5878042c63bf9c7272
Renildo15 <renildorabi@ufrn.edu.br>
2025-01-29 15:54:09 -0300

    feat: buscando dados da api

--- components/home/clubs-list/index.tsx
+++ components/home/clubs-list/index.tsx
@@ -1,17 +1,47 @@
-import { FlatList } from "react-native";
+import { FlatList, RefreshControl } from "react-native";
 import CardNormal from "../card-normal";
-import { View } from "@/components/Themed";
+import { Text, View } from "@/components/Themed";
 import { home } from "@/data/home";
+import { UseClubs } from "@/hooks/useClubs";
+import { useState } from "react";
 
 export default function ClubsList() {
+    const {
+        result,
+        isError,
+        isLoading,
+        refetch
+    } = UseClubs();
+
+    const [refreshing, setRefreshing] = useState(false);
+
+    const onRefresh = async () => {
+        setRefreshing(true);
+        await refetch();
+        setRefreshing(false);
+      };
+    
     return (
         <View style={{width: '100%'}}>
-            <FlatList
-                data={home}
-                renderItem={({item}) => <CardNormal id={item.id} name={item.name} federation={item.federation}/>}
-                keyExtractor={item => item.id}
-                contentContainerStyle={{ paddingBottom: 70 }}
-            />
+            { isError ? (
+                <Text>{isError.message}</Text>
+            ) : isLoading ? (
+                <Text>Carregando</Text>
+            ) : (
+                <FlatList
+                    data={result.results}
+                    renderItem={({item}) => <CardNormal id={item.id} name={item.name} federation={item.federation} uri={item.emblem_versions.original}/>}
+                    keyExtractor={item => item.id}
+                    contentContainerStyle={{ paddingBottom: 70 }}
+                    refreshControl={
+                        <RefreshControl
+                          refreshing={refreshing}
+                          onRefresh={onRefresh}
+                          colors={['#007bff']}
+                        />
+                      }
+                />
+            )}
         </View>
     )
 }
