db43a9c3c0e4c00adeb45d6a06531ec2f9d3ce93
Renildo15 <renildorabi@ufrn.edu.br>
2025-01-29 15:54:37 -0300

    feat: configgurando swr

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,4 +1,5 @@
 import FontAwesome from '@expo/vector-icons/FontAwesome';
+import { SWRConfig } from 'swr';
 import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
 import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
@@ -8,6 +9,9 @@ import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
 import { StatusBar } from 'expo-status-bar';
+import { fetcher } from '@/services/api';
+// import { swrStorageProvider } from '@/utils/swr-storage-provider';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 export {
   // Catch any errors thrown by the Layout component.
@@ -21,6 +25,21 @@ export const unstable_settings = {
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
+
+const cache = new Map();
+
+async function loadCacheFromStorage() {
+  const storedCache = await AsyncStorage.getItem("swr-cache");
+  if (storedCache) {
+      const parsedCache = JSON.parse(storedCache);
+      parsedCache.forEach(([key, value]: [String, any]) => {
+        cache.set(key, value)
+      })
+  }
+
+}
+
+loadCacheFromStorage();
 
 export default function RootLayout() {
   const [loaded, error] = useFonts({
@@ -50,13 +69,28 @@ function RootLayoutNav() {
   const colorScheme = useColorScheme();
 
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <StatusBar style="auto" />
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="search" options={{ presentation: 'modal', title:"Busca" }} />
-        <Stack.Screen name="settings" options={{ presentation: 'modal', title:"Configurações"}} />
-      </Stack>
-    </ThemeProvider>
+    <SWRConfig
+      value={{
+        fetcher,
+        provider: () => cache,
+        onErrorRetry: (error, key, config, revalidate, { retryCount}) => {
+          if (retryCount >= 3) return;
+          setTimeout(() => revalidate({retryCount}), 2000)
+        },
+        onSuccess: async (data, key) => {
+          const cacheArray = Array.from(cache.entries());
+          await AsyncStorage.setItem("swr-cache", JSON.stringify(cacheArray))
+        }
+      }}
+    >
+       <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+          <StatusBar style="auto" />
+          <Stack>
+            <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+            <Stack.Screen name="search" options={{ presentation: 'modal', title:"Busca" }} />
+            <Stack.Screen name="settings" options={{ presentation: 'modal', title:"Configurações"}} />
+          </Stack>
+      </ThemeProvider>
+    </SWRConfig>
   );
 }
