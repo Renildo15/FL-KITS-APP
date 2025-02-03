ab81f722d40fe4803add1584cd4a5172a9a94530
Renildo15 <renildorabi@ufrn.edu.br>
2025-02-03 11:35:34 -0300

    feat:criando hook de add kit

---
+++ hooks/useAddKit.ts
@@ -0,0 +1,34 @@
+import { api } from "@/services/api";
+import axios from "axios";
+
+export async function AddKit(newKit: FormData) {
+    interface IResponse {
+    message: string;
+    }
+
+    try {
+    const res = await api.post<IResponse>("/api/kit/create/", newKit, {
+        headers: {
+            'Content-Type': 'multipart/form-data',
+        }
+    });
+
+    return res.data;
+    } catch (error) {
+        if (axios.isAxiosError(error)) {
+            console.debug('Axios error message:', error.message);
+            if (error.response) {
+                console.debug('Response data:', error.response.data);
+                console.debug('Response status:', error.response.status);
+                console.debug('Response headers:', error.response.headers);
+            } else if (error.request) {
+                console.error('Request data:', error.request);
+            } else {
+                console.error('Error setting up request:', error.message);
+            }
+        } else {
+            console.error('Unexpected error:', error);
+        }
+        throw error;
+    }
+}
